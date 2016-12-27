0b7542bb216e833804471490766c159cbab7a67a
Tom Smeding <tom.smeding@gmail.com>
2016-12-28 00:06:53 +0100

    More locate!

--- locate2.js
+++ locate2.js
@@ -8,9 +8,11 @@ function avgAngle(d1,d2){
 	return Math.atan2(ry,rx);
 }
 
-// Takes list of directions in degrees;
-// Returns list of [x,y] positions.
-function computeLocations(dirs){
+function wrap(x,l,h){
+	return ((x-l)%h+h)%h+l;
+}
+
+function computePreliminary(dirs){
 	var N=dirs.length;
 	if(N==0){
 		return [];
@@ -39,7 +41,8 @@ function computeLocations(dirs){
 		var dis=-dx*ey+dy*ex;
 		console.log("i="+i+" dis="+dis);
 		if(Math.abs(dis)<0.4){
-			throw Error("People too close together!");
+			poses[i]=[Math.random(),Math.random()-0.5];
+			continue;
 		}
 		var t=-ey/dis,s=-dy/dis;
 		console.log("i="+i+" t="+t+" s="+s);
@@ -55,6 +58,55 @@ function computeLocations(dirs){
 		rx=Math.cos(goffset)*poses[i][0]-Math.sin(goffset)*poses[i][1];
 		ry=Math.sin(goffset)*poses[i][0]+Math.cos(goffset)*poses[i][1];
 		poses[i]=[rx,ry];
+	}
+
+	return poses;
+}
+
+function computeScore(dirs,poses){
+	var N=dirs.length;
+	var sc=0;
+	var i,j;
+	var compdir,s;
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			if(i==j){
+				continue;
+			}
+			compdir=Math.atan2(poses[j][1]-poses[i][1],poses[j][0]-poses[i][0]);
+			s=Math.abs(wrap(dirs[i][j]-compdir+Math.PI,0,2*Math.PI)-Math.PI)/Math.PI;
+			sc+=s;
+		}
+	}
+	return sc;
+}
+
+var NITERS=10;
+var INCREMENT=0.1;
+var STEP=0.05;
+
+// Takes list of directions in degrees;
+// Returns list of [x,y] positions.
+function computeLocations(dirs){
+	var N=dirs.length;
+	var poses=computePreliminary(dirs);
+	var diffs=new Array(N);
+	var iter,i,j,diffx,diffy,incr;
+	var initScore;
+	for(iter=0;iter<NITERS;iter++){
+		initScore=computeScore(dirs,poses);
+		console.log("initScore="+initScore);
+		incr=INCREMENT-iter/NITERS*(INCREMENT/2);
+		for(i=0;i<N;i++){
+			poses[i][0]+=incr; diffx=computeScore(dirs,poses)-initScore; poses[i][0]-=incr;
+			poses[i][1]+=incr; diffy=computeScore(dirs,poses)-initScore; poses[i][1]-=incr;
+			diffs[i]=[diffx,diffy];
+		}
+
+		i=Math.random()*N|0;
+		j=Math.round(Math.random());
+		console.log("diffs["+i+"]["+j+"] = "+diffs[i][j]);
+		poses[i][j]-=(STEP-iter/NITERS*(STEP/2))*diffs[i][j];
 	}
 
 	return poses;
