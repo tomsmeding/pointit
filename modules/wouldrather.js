67d94755577d00639bcb0b9fb291683bdb3930f2
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-04-07 20:09:28 +0200

    make wouldrather somewhat working

--- modules/wouldrather.js
+++ modules/wouldrather.js
@@ -11,29 +11,62 @@ const questions = [
 module.exports = class WouldRather extends Module {
 	constructor(options) {
 		super(options);
+		this.current = -1;
+		this.next();
 	}
 
 	next() {
 		this.current++;
+		this.ready = false;
+		this.callbacks = []; // REVIEW
+		this.answers = [];
+		return this.current < questions.length;
 	}
 
 	getCurrent(player) {
 		return {
 			question: questions[this.current],
-			answers: this.players.filter(p => p.id !== player.id),
+			answers: this._players(player),
 		};
 	}
 
-	async checkAnswer(player, answerId) {
-		throw new Error('not implemented');
+	_ready() {
+		return this.answers.length !== this.game.activePlayers().length;
+	}
 
-		this.answers.push(answer);
+	_checkAnswer(player) {
+		const pair = _.chain(this.answers)
+			.groupBy('answerId')
+			.pairs()
+			.max(pair => pair[1].length)
+			.value();
+		const answerId = this.getAnswer(player);
+		return pair[1] === answerId;
+	}
 
-		const promise = new Promise((resolve, reject) => {
-			this.callbacks.push([ resolve, reject ]);
+	provideAnswer(options) {
+		super.provideAnswer(options);
+
+		if (this._ready()) {
+			for (const { player, resolve } of this.callbacks) {
+				resolve(this._checkAnswer(player));
+			}
+			this.callbacks = [];
+		}
+	}
+
+	async checkAnswer({ player }) {
+		if (this._ready()) {
+			return this._checkAnswer(player);
+		}
+
+		return new Promise((resolve, reject) => {
+			this.callbacks.push({
+				player,
+				resolve,
+				reject,
+			});
 		});
-
-		return promise;
 	}
 }
 
